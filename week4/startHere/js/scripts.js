f3c35b6e9f2def319bf234713c18711f258f3947
Cale Bodily <cbodily1@byupathway.edu>
2026-09-23 13:30:27 -0600

    animation and advanced form activites

---
+++ week4/startHere/js/scripts.js
@@ -0,0 +1,7 @@
+const myinfo = new URLSearchParams(window.location.search);
+// console.log(myinfo);
+
+document.querySelector('#results').innerHTML = `<p>Appointment for ${myinfo.get('first')} ${myinfo.get('last')}</p>
+<p>Proxy ${myinfo.get('ordinance')} on ${myinfo.get('date')} in the ${myinfo.get('location')} Temple</p>
+<p>Your Phone: ${myinfo.get('phone')} </p>
+<p>Your email is ${myinfo.get('email')} </p>`
